606bf302b8bc7619ede2418a92bf490c81d6b7eb
Tsinswreng <2290019838@qq.com>
2024-10-26 20:32:01 +0800

    增.editorconfig; 試多語言

--- ngaq/App.axaml.cs
+++ ngaq/App.axaml.cs
@@ -6,36 +6,64 @@ using Avalonia.Markup.Xaml;
 using ngaq.ViewModels;
 using ngaq.Views;
 
-namespace ngaq
+namespace ngaq {
+	public partial class App : Application {
+		public override void Initialize() {
+			AvaloniaXamlLoader.Load(this);
+		}
+
+		public override void OnFrameworkInitializationCompleted() {
+			if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+				// Line below is needed to remove Avalonia data validation.
+				// Without this line you will get duplicate validations from both Avalonia and CT
+				BindingPlugins.DataValidators.RemoveAt(0);
+				desktop.MainWindow = new MainWindow {
+					DataContext = new MainViewModel()
+				};
+			}
+			else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform) {
+				singleViewPlatform.MainView = new MainView {
+					DataContext = new MainViewModel()
+				};
+			}
+
+			base.OnFrameworkInitializationCompleted();
+		}
+	}
+}
+
+/* 
+<TextBlock Text="{DynamicResource AppName}" />
+<TextBlock Text="{DynamicResource HelloWorld}" />
+<TextBlock Text="{DynamicResource Greeting}" />
+
+
+using Avalonia.Markup.Xaml.Styling;
+using System.Globalization;
+
+// ... other code ...
+
+public class App : Application
 {
-    public partial class App : Application
+    public override void Initialize()
     {
-        public override void Initialize()
+        AvaloniaXamlLoader.Load(this);
+        var culture = CultureInfo.CurrentCulture;
+        var resourcePath = $"Resources/Strings.{culture.Name}.xaml";
+
+        //尝试加载特定语言的资源，如果失败则加载默认资源
+        try
         {
-            AvaloniaXamlLoader.Load(this);
+            var style = new StyleInclude(new Uri("avares://YourAppName/Styles.xaml")) { Source = new Uri(resourcePath) };
+            Styles.Add(style);
         }
-
-        public override void OnFrameworkInitializationCompleted()
+        catch
         {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                // Line below is needed to remove Avalonia data validation.
-                // Without this line you will get duplicate validations from both Avalonia and CT
-                BindingPlugins.DataValidators.RemoveAt(0);
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainViewModel()
-                };
-            }
-            else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
-            {
-                singleViewPlatform.MainView = new MainView
-                {
-                    DataContext = new MainViewModel()
-                };
-            }
-
-            base.OnFrameworkInitializationCompleted();
+            var style = new StyleInclude(new Uri("avares://YourAppName/Styles.xaml")) { Source = new Uri("avares://YourAppName/Resources/Strings.xaml") };
+            Styles.Add(style);
         }
     }
+
+    // ... other code ...
 }
+ */
--- ngaq/ViewModels/MainViewModel.cs
+++ ngaq/ViewModels/MainViewModel.cs
@@ -1,17 +1,57 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml.Converters;
+using System.Globalization;
+using Avalonia;
 namespace ngaq.ViewModels;
 
 
 public partial class MainViewModel : ViewModelBase{
-	// 類於vue之ref?
-	[ObservableProperty]
-	private string greeting = "greeting";
+	public MainViewModel(){
+
+	}
 
 	//private string _greetings = "一二三四五六七八九十Welcome to Avalonia!"; //不可、名ˋ須潙_greeting
 	//private string _greeting = "一二三四五六七八九十Welcome to Avalonia!";
 	//private string ____greeting = "____greeting";
+	// private CultureInfo _currentCulture = CultureInfo.CurrentUICulture;
+	// public CultureInfo CurrentCulture{
+	// 	get => _currentCulture;
+	// 	set{
+	// 		if(value == _currentCulture) return;
+	// 		_currentCulture = value;
+	// 		//OnPropertyChanged(nameof(CurrentCulture));
+	// 		OnPropertyChanged();//通知UI更新
+	// 		ReloadResources();
+	// 	}
+	// }
 
+	[ObservableProperty]
+	private string _greeting = "greeting";
+	// public string Greeting{
+	// 	get => _greeting;
+	// 	set => this.RaiseAndSetIfChanged(ref _greeting, value);
+	// }
+
+
+	private string _buttonText;
+	// public string ButtonText{
+	// 	get => _buttonText;
+	// 	set => this.RaiseAndSetIfChanged(ref _buttonText, value);
+	// }
+
+
+
+	// private void ReloadResources(){
+	// 	var resource = AvaloniaLocator.Current.GetService<IResourceProvider>();
+	// 	var resources = resource.GetResources(_currentCulture);
+	// 	Greeting = resources["Greeting"] as string;
+	// 	ButtonText = resources["ButtonText"] as string;
+	// }
+
+	// public void SwitchLanguage(){
+	// 	CurrentCulture = CurrentCulture.Name == "zh-CN" ? new CultureInfo("en-US") : new CultureInfo("zh-CN");
+	// }
 }
 
 /* 
