469684b3045bfaf6cb8a9d5f3738d1d42cc2280e
Tsinswreng <2290019838@qq.com>
2024-12-05 13:49:16 +0800

    ngaq.Common改ngaq.Core

--- ngaq.Browser/Program.cs
+++ ngaq.Browser/Program.cs
@@ -1,16 +1,15 @@
-﻿using Avalonia;
+﻿using System.Runtime.Versioning;
+using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.Browser;
 using ngaq;
-using System.Runtime.Versioning;
-using System.Threading.Tasks;
 
-[assembly: SupportedOSPlatform("browser")]
+internal sealed partial class Program
+{
+    private static Task Main(string[] args) => BuildAvaloniaApp()
+            .WithInterFont()
+            .StartBrowserAppAsync("out");
 
-internal sealed partial class Program{
-	private static Task Main(string[] args) => BuildAvaloniaApp()
-			.WithInterFont()
-			.StartBrowserAppAsync("out");
-
-	public static AppBuilder BuildAvaloniaApp()
-		=> AppBuilder.Configure<App>();
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>();
 }
