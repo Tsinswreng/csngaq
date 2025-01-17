aa284833613096483628eeb8044f6919b38cd522
Tsinswreng <2290019838@qq.com>
2025-01-17 14:46:48 +0800

    用nc.xml

--- ngaq.Core/GlobalUsing.cs
+++ ngaq.Core/GlobalUsing.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CS8981
 global using std = System;
 global using u8 = System.Byte;
 global using i32 = System.Int32;
@@ -12,14 +13,3 @@ global using str = System.String;
 global using unknown = System.Object;
 global using any = System.Dynamic.ExpandoObject;
 global using System.Threading.Tasks;
-
-//global using Map = System.Collections.Generic.Dictionary;
-//global using Map<K,V> = System.Collections.Generic.Dictionary<K,V>;
-
-
-
-/*
-用str表示相當于js之string
-用String表示相當於js之String
-勿用new String()、須用字面量
- */
