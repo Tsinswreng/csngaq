b7a5804d4d44f2e53938efe6e58a0ff143b271d1
Tsinswreng <2290019838@qq.com>
2025-02-11 13:04:39 +0800

    製UnixMsConverter

--- ngaq.UI/Converter/UnixMsConverter.cs
+++ ngaq.UI/Converter/UnixMsConverter.cs
@@ -2,19 +2,22 @@ using System;
 using System.Globalization;
 using Avalonia;
 using Avalonia.Data.Converters;
+using Shr.Date;
 namespace ngaq.UI.Converter;
 
 public class UnixMsConverter : IValueConverter {
 	public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
 		if(value is i64 unixMs){
-			
+			return DateUtil.unixMsToIso8601(unixMs);//str
 		}
-
 		return AvaloniaProperty.UnsetValue;
 	}
 
 	public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
-		throw new NotImplementedException();
+		if(value is str iso8601){
+			return DateUtil.iso8601ToUnixMs(iso8601);//i64
+		}
+		return AvaloniaProperty.UnsetValue;
 	}
 }
 
